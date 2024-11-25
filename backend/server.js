e6f70e62f7b0496e6bc1f5f65345ad64a302499d
SanyamBhatia <122440353+SanyamBhatia@users.noreply.github.com>
2024-11-25 11:52:37 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,8 @@ import cartRoutes from "./routes/cart.route.js";
 import couponRoutes from "./routes/coupon.route.js";
 import paymentRoutes from "./routes/payment.route.js";
 import analyticsRoutes from "./routes/analytics.route.js";
+import path from "path";
+
 const app = express();
 
 const PORT = process.env.PORT || 5001;
